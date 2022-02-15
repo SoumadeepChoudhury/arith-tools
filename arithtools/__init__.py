8489c3e218b29f2e6e73222798c333c3378489ec
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-02-16 01:26:03 +0530

    Updated to v1.1.1

--- arithtools/__init__.py
+++ arithtools/__init__.py
@@ -1,3 +1,4 @@
+
 def fibonacci_series(range, num1=0, num2=1) -> int:
     """
         Takes <range> from user and default parameter value is 0 and 1 and prints the fibonacci series in order.
@@ -226,49 +227,49 @@ def boiled_egg(number) -> str:
         else:
             print(li[t - 7*z])
             z += 1
-        
+
+
 def auto_biographical(number) -> bool:
     """Check whether the entered number is auto_biographical number or not."""
-    n=number
-    sum=0
-    ct=0
-    while(n!=0):
-        r=n%10
-        ct+=1
-        sum+=r
-        n=n//10
-    if(sum==ct):
+    n = number
+    sum = 0
+    ct = 0
+    while(n != 0):
+        r = n % 10
+        ct += 1
+        sum += r
+        n = n//10
+    if(sum == ct):
         return True
     else:
         return False
 
-  
+
 def pascal_triangle(number):
     """User will enter the row and it will print the pascal's triangle along with space."""
-    for i in range(0,number):
-        for k in range(number,i,-1):
+    for i in range(0, number):
+        for k in range(number, i, -1):
             print(end=' ')
-        num=1
-        for j in range(0,i+1):
-            print(num,end=' ')
-            num=num*(i-j)//(j+1)
+        num = 1
+        for j in range(0, i+1):
+            print(num, end=' ')
+            num = num*(i-j)//(j+1)
         print()
-
 
 
 def two_digit_special(number) -> bool:
     """It will check whether the entered number is a two digit special number or not."""
-    s=0
-    n=number
-    p=1
-    a=n
-    if(n>9 and n<100):
-        while(n!=0):
-            r=n%10
-            p=p*r
-            s=s+r
-            n=n//10
-        if(s+p==a):
+    s = 0
+    n = number
+    p = 1
+    a = n
+    if(n > 9 and n < 100):
+        while(n != 0):
+            r = n % 10
+            p = p*r
+            s = s+r
+            n = n//10
+        if(s+p == a):
             return True
         else:
             return False
@@ -278,17 +279,17 @@ def two_digit_special(number) -> bool:
 
 def krishnamurti(number) -> bool:
     """It will check whether the entered number is a krishnamurti number or not."""
-    s=0
-    n=number
-    a=n
-    while(n!=0):
-        f=1
-        r=n%10
-        for i in range(1,r+1):
-            f=f*i
-        s=s+f
-        n=n//10
-    if(s==a):
+    s = 0
+    n = number
+    a = n
+    while(n != 0):
+        f = 1
+        r = n % 10
+        for i in range(1, r+1):
+            f = f*i
+        s = s+f
+        n = n//10
+    if(s == a):
         return True
     else:
         return False
@@ -296,13 +297,13 @@ def krishnamurti(number) -> bool:
 
 def pronic(number) -> bool:
     """It will check whether the entered number is a pronic number."""
-    flag=0
-    n=number
-    for i in range(0,n):
-        if(i*(i+1)==n):
-            flag=1
+    flag = 0
+    n = number
+    for i in range(0, n):
+        if(i*(i+1) == n):
+            flag = 1
             break
-    if(flag==1):
+    if(flag == 1):
         return True
     else:
         return False
@@ -310,20 +311,20 @@ def pronic(number) -> bool:
 
 def darsium(number) -> bool:
     """It will check whether the entered number is a darsium number or not."""
-    s=0
-    n=number
-    c=0
-    p=n
-    x=n
-    while(p!=0):
-        p=p//10
-        c+=1
-    while(n!=0):
-        r=n%10
-        s=s+(r**c)
-        c=c-1
-        n=n//10
-    if(s==x):
+    s = 0
+    n = number
+    c = 0
+    p = n
+    x = n
+    while(p != 0):
+        p = p//10
+        c += 1
+    while(n != 0):
+        r = n % 10
+        s = s+(r**c)
+        c = c-1
+        n = n//10
+    if(s == x):
         return True
     else:
         return False
@@ -331,26 +332,23 @@ def darsium(number) -> bool:
 
 def happy(number) -> bool:
     """It will check whther the entered number is a happy one or not"""
-    p=0
-    n=number
-    if(n>0 and n<10):
+    p = 0
+    n = number
+    if(n > 0 and n < 10):
         return False
     else:
         while True:
-            s=0
-            while(n!=0):
-                r=n%10
-                s=s+(r*r)
-                n=n//10
-            if(s<10):
-                p=s
+            s = 0
+            while(n != 0):
+                r = n % 10
+                s = s+(r*r)
+                n = n//10
+            if(s < 10):
+                p = s
                 break
             else:
-                n=s
-        if(p==1):
+                n = s
+        if(p == 1):
             return True
         else:
             return False
-
-
-            
--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="arith-tools",
-    version="1.1.0",
+    version="1.1.1",
     description="Use all the simple arithmetic calculations such as fibonacci series, armstrong number etc.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/SoumadeepChoudhury/arith-tools",
     author="Ahens | An Initiative to Initial (Soumadeep Choudhury)",
     author_email="ahensinitiative@gmail.com",
+    maintainer="Manmay Chakraborty",
+    maintainer_email="manmaychakarborty@gmail.com",
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
